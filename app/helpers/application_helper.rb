92ae489e4a93cbb074902913c1442444b066309d
Santiago Almiron <sanalmiron@hotmail.com>
2024-07-27 17:04:51 -0300

    install rubocop

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
+# Default helper generated
 module ApplicationHelper
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # I dont know whats this is for.
   get 'up' => 'rails/health#show', as: :rails_health_check
 
-  root :to => 'home#index'
+  root to: 'home#index'
 
   resources :home, only: :index
 end
